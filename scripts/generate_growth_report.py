b27dacd3a7ce8579ad2c137c3534a75a5453027e
Helen G Scott <helengracescott@gmail.com>
2026-05-24 15:03:49 -0400

    feat: clean up the saved table for use in the paper

--- scripts/generate_growth_report.py
+++ scripts/generate_growth_report.py
@@ -77,12 +77,8 @@ def generate_growth_phenotype_report(model: cobra.Model):
     growth_phenotypes["pred_growth"] = pred_growth
     growth_phenotypes["fba_growth_rate"] = fba_growth_rate
 
-    # Save the dataframe as a TSV
-    growth_phenotypes.to_csv(
-        os.path.join(RESULTS_DIR, "known_growth_phenotypes_w_pred.tsv"),
-        sep="\t",
-        index=False,
-    )
+    # Beautify and save the table
+    beautify_table(growth_phenotypes)
 
     # Plot a categorical heatmap of the growth phenotypes, where the rows
     # are the metabolites and the columns are the experimental and predicted
@@ -171,6 +167,73 @@ def generate_growth_phenotype_report(model: cobra.Model):
     plt.savefig(os.path.join(RESULTS_DIR, "exp_vs_pred_growth_phenotypes.png"))
 
 
+def beautify_table(exp_pred_table: pd.DataFrame):
+    # Get all of the unique minimal media in the table
+    unique_minimal_media = exp_pred_table["minimal_media"].unique()
+    # Make a dictionary where the keys are the minimal media and the values are the nitrogen-containing compounds in that media, separated by commas
+    n_dict = {}
+    # For each unique minimal media, look up the nitrogen-containing compounds in that media and add a column to the table with that information
+    for minimal_media in unique_minimal_media:
+        # Get the media definition for that minimal media
+        media_def = media_definitions[minimal_media]
+        # Get the nitrogen-containing compounds in that media definition
+        n_containing_compounds = []
+        for ex_rxn in media_def:
+            # Get the metabolite ID from the reaction ID
+            # (remove the "EX_" prefix)
+            met_id = ex_rxn[3:]  # Remove "EX_"
+            # Try to get the metabolite object from the model
+            try:
+                met = model.metabolites.get_by_id(met_id)
+            except KeyError:
+                continue
+            # Check if the metabolite contains nitrogen in its formula
+            if "N" in met.elements:
+                # Ignore vitamins (thiamin and vitamin B12)
+                if met.id in ["cpd00305_e0", "cpd03424_e0"]:
+                    continue
+                # If it does, add the name of the metabolite to the list of nitrogen-containing compounds
+                # Remove the " [e0]" from the end of the name, if it exists
+                if met.name.endswith(" [e0]"):
+                    met_name = met.name[:-5]
+                else:
+                    met_name = met.name
+                n_containing_compounds.append(met_name)
+        # Store the list of nitrogen-containing compounds for this minimal media
+        # as a string separated by commas
+        n_dict[minimal_media] = ", ".join(n_containing_compounds)
+    # Add a column to the table with the nitrogen-containing compounds, separated by commas
+    exp_pred_table["Medium N Source(s)"] = exp_pred_table["minimal_media"].map(n_dict)
+    # Replacing the media names in the minimal_media column with the human-friendly versions
+    exp_pred_table["minimal_media"] = exp_pred_table["minimal_media"].map(media_names)
+    # Subset the columns we want
+    exp_pred_table = exp_pred_table[
+        [
+            "minimal_media",
+            "Medium N Source(s)",
+            "c_source",
+            "growth",
+            "fba_growth_rate",
+        ]
+    ]
+    # Sort the table by the minimal media and then the carbon source
+    exp_pred_table = exp_pred_table.sort_values(by=["minimal_media", "c_source"])
+    # And rename the columns to be more descriptive
+    exp_pred_table = exp_pred_table.rename(
+        columns={
+            "minimal_media": "Minimal Media",
+            "c_source": "Added Metabolite(s)",
+            "growth": "Experimental Growth",
+            "fba_growth_rate": "FBA Predicted Growth Rate",
+        }
+    )
+    # Save
+    exp_pred_table.to_csv(
+        os.path.join(RESULTS_DIR, "known_growth_phenotypes_w_pred.csv"),
+        index=False,
+    )
+
+
 def generate_biomass_producibility_report(model: cobra.Model):
     # Load the TSV of the growth phenotypes
     growth_phenotypes = pd.read_csv(
@@ -203,4 +266,5 @@ if __name__ == "__main__":
 
     # Generate the reports
     generate_growth_phenotype_report(model)
-    generate_biomass_producibility_report(model)
+    # TODO: Un-comment this!
+    # generate_biomass_producibility_report(model)
