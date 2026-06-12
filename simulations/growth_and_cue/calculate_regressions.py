4b34d9d25de43e30205b9fdfd8cb337fd37a44dd
Helen G Scott <helengracescott@gmail.com>
2026-06-12 09:51:10 -0400

    feat: print atp_cost per group

--- simulations/growth_and_cue/calculate_regressions.py
+++ simulations/growth_and_cue/calculate_regressions.py
@@ -26,6 +26,9 @@ def main():
     # Merge the results with the substrate info
     merged_df = pd.merge(results_df, substrate_df, on="met_id")
 
+    # For debugging
+    print(merged_df.groupby("entry_point_x")[["atp_cost", "growth_rate"]].mean())
+
     # Fit a regression line for growth rate and atp_cost
     m_growth_v_atp, d_growth_v_atp = fit(merged_df, "growth_rate", ["atp_cost"])
     m_cue_v_atp, d_cue_v_atp = fit(merged_df, "cue", ["atp_cost"])
