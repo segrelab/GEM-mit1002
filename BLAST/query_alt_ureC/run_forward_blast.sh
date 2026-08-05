19c127f16630cb37ad22fb5dd92a94765983e7ec
Helen G Scott <helengracescott@gmail.com>
2026-08-05 14:36:18 -0400

    fix: correct the comment of what the queries are

--- BLAST/query_alt_ureC/run_forward_blast.sh
+++ BLAST/query_alt_ureC/run_forward_blast.sh
@@ -1,5 +1,5 @@
 # Add a header to the output file
 printf 'qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore\n' > query_alt_ureC/results/forward_blast_hits.tsv
 
-# Run BLASTp of PG1 dut sequence against the RefSeq genome database
+# Run BLASTp of multiple Alteromonas ureC sequences against the RefSeq genome database
 blastp -query query_alt_ureC/alt_ureC_seqs.fa -db dbs/amac_refseq_db  -outfmt 6 -max_target_seqs 10 >> query_alt_ureC/results/forward_blast_hits.tsv
